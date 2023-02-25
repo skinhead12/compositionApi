0921f95d0e436c7be9b08f727629ae2f549053b2
skinhead12 <edithr_isc@hotmail.com>
2023-02-25 11:44:22 -0600

    usando fetch y hooks cycle

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <PostList :posts="posts" />
+    <div v-if="error"> {{ error }}</div>
+    <div v-if="posts.length"> 
+      <PostList v-if="showPosts" :posts="posts" />
+    </div>
+    <div v-else>Loading...</div>
+    <button @click="showPosts = !showPosts">toggle posts</button>
+    <button @click="posts.pop()">delete a post</button>
   </div>
 </template>
 
@@ -13,11 +19,29 @@ export default {
   name: 'Home',
   components: { PostList },
   setup(){
-    const posts = ref ([
+   /*  const posts = ref ([
       { title:'welcome in the blog', body:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque animi explicabo atque sed, perspiciatis, expedita deserunt impedit corrupti dicta, cum aliquid natus architecto temporibus? Cum beatae totam corrupti id eius.', id: 1 },
       { title:'top 5 css tips', body:'lorem ipsum', id: 2 },
-    ])
-    return { posts }
+    ]) */
+    const posts = ref ([])
+    const error = ref(null)
+
+    const load = async () => {
+      try {
+        let data = await fetch('http://localhost:3000/posts')
+        if (!data.ok){
+          throw Error('no data available')
+        }
+        posts.value = await data.json()
+      }
+      catch (err){
+        error.value = err.message
+        console.log(err.value);
+      }
+    }
+    load()
+    
+    return { posts, error }
   }
 }
 </script>
